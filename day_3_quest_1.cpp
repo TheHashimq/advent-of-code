f44f38a60e2faa9dad6512ae1b3f97fd58cc8125
thehashimq <hashimq905@gmail.com>
2024-12-04 01:16:17 +0530

    done

--- day_3_quest_1.cpp
+++ day_3_quest_1.cpp
@@ -37,13 +37,11 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-// Improved validateMul function
 std::vector<std::string> validateMul(const std::vector<std::string> &mulList) {
   std::vector<std::string> validatedList;
   std::regex mulRegex(R"(\d+,\d+)");
 
   for (const auto &mul : mulList) {
-    // Extract content within parentheses
     size_t start = mul.find('(');
     size_t end = mul.find(')');
     if (start != std::string::npos && end != std::string::npos &&
@@ -58,7 +56,6 @@ std::vector<std::string> validateMul(const std::vector<std::string> &mulList) {
   return validatedList;
 }
 
-// Improved get_value_from_mul function
 std::vector<int>
 get_value_from_mul(const std::vector<std::string> &validatedList) {
   std::vector<int> multiples;
@@ -78,64 +75,16 @@ get_value_from_mul(const std::vector<std::string> &validatedList) {
 
 std::vector<std::string> findMul(const std::string &inp) {
   std::vector<std::string> res;
-  size_t pos = 0;
 
-  while (pos < inp.size()) {
-    size_t start = inp.find("mul(", pos);
-    if (start == std::string::npos) {
-      break;
-    }
-    size_t end = inp.find(')', start);
-    if (end == std::string::npos) {
-      std::cerr << "Warning: Unmatched 'mul(' at position " << start << '\n';
-      break;
-    }
+  // Regular expression to match "mul(x,y)" where x and y are integers
+  std::regex mulRegex(R"(mul\(\d+,\d+\))");
+  std::sregex_iterator begin(inp.begin(), inp.end(), mulRegex);
+  std::sregex_iterator end;
 
-    std::string mul_string = inp.substr(start, end - start + 1);
-    std::cout << "Pushing MUl _String " << mul_string << '\n';
-    res.push_back(mul_string);
-    pos = end + 1;
+  for (auto it = begin; it != end; ++it) {
+    res.push_back(it->str());
+    std::cout << "Pushing MUL_String " << it->str() << '\n';
   }
 
   return res;
 }
-
-/*
-std::vector<std::string> validateMul(std::vector<std::string> &mulList) {
-  std::vector<std::string> validatedList{};
-  for (auto mul : mulList) {
-    mul = mul.substr(4, mul.find(')'));
-    auto cstr = mul.c_str();
-    auto size = mul.size();
-    bool isValid{};
-    for (int i = 0; i < size; i++) {
-      if (isdigit(cstr[i]) || cstr[i] == ',' || cstr[i] == ')') {
-        isValid = true;
-      } else {
-        isValid = false;
-        break;
-      }
-    }
-    if (isValid) {
-      validatedList.push_back(mul);
-    }
-  }
-  return validatedList;
-}
-*/
-/*
-std::vector<int>
-get_value_from_mul(const std::vector<std::string> &validatedList) {
-  std::vector<int> multiple;
-
-  for (auto v : validatedList) {
-    std::stringstream ss(v);
-    int num1{}, num2{};
-    char delimiter{}, endChar{};
-    ss >> num1 >> delimiter >> num2 >> endChar;
-    multiple.push_back((num1 * num2));
-  }
-  return multiple;
-}
-
-*/
